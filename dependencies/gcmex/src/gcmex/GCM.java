60af8b48702d78750de9f820ebde11f31644759a
Federico Bricker <fbricker@gmail.com>
2013-12-28 15:10:17 -0300

    Done with registration. Working on SendMessage

--- dependencies/gcmex/src/gcmex/GCM.java
+++ dependencies/gcmex/src/gcmex/GCM.java
@@ -30,21 +30,11 @@ import org.haxe.extension.Extension;
 
 public class GCM extends Extension {
 
-	public static final String EXTRA_MESSAGE = "message";
-	public static final String PROPERTY_REG_ID = "registration_id";
-	private static final String PROPERTY_APP_VERSION = "appVersion";
-	private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-	private static final String TAG = "GCMDemo";
-
-	private static String SENDER_ID = "Your-Sender-ID";
-
-    GoogleCloudMessaging gcm;
-    AtomicInteger msgId = new AtomicInteger();
-    SharedPreferences prefs;
-    Context context;
-    String regid;
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public static boolean checkPlayServices() {
+		Log.i(TAG, "CHECK PLAY SERVICES.");
 		try{
 			int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(mainActivity);
 			if (resultCode == ConnectionResult.SUCCESS) return true;
@@ -59,34 +49,48 @@ public class GCM extends Extension {
 		}
 	}
 
-	@Override
-	public void onResume() {
-	    super.onResume();
-	    checkPlayServices();
+	public static String getRegistrationId_Static() {
+		return getRegistrationId(mainContext);
 	}
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		if (checkPlayServices()) {
+	public static void init(String senderId){
+		SENDER_ID=senderId;
+		if(gcm==null){
+			getAppVersion(mainContext);
+			Log.i(TAG, "CREATE GCM INSTANCE.");
 			gcm = GoogleCloudMessaging.getInstance(mainActivity);
-            regid = getRegistrationId(mainContext);
-            if (regid.isEmpty()) {
-                registerInBackground();
-            }
-	    }
+			regid = getRegistrationId(mainContext);
+			if (regid.isEmpty()) {
+				registerInBackground();
+			}
+		}
 	}
 
-	/**
-	 * Registers the application with GCM servers asynchronously.
-	 * <p>
-	 * Stores the registration ID and app versionCode in the application's
-	 * shared preferences.
-	 */
-	private void registerInBackground() {
-	    new AsyncTask() {
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	public static final String EXTRA_MESSAGE = "message";
+	public static final String PROPERTY_REG_ID = "registration_id";
+	private static final String PROPERTY_APP_VERSION = "appVersion";
+	private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+	private static final String TAG = "OPENFL-GCM";
+
+	private static String SENDER_ID = "MUST CALL INIT TO SET SENDER ID!";
+
+	static GoogleCloudMessaging gcm = null;
+	static AtomicInteger msgId = new AtomicInteger();
+	static SharedPreferences prefs;
+	static Context context;
+	static String regid;
+	static int currentAppVersion=0;
+
+	private static void registerInBackground() {
+		Log.i(TAG, "registerInBackground. SenderID: "+SENDER_ID);
+	    new AsyncTask<String,String,String>() {
 			@Override
-	        protected String doInBackground(Object... args) {
+	        protected String doInBackground(String... args) {
+				Log.i(TAG, "AsyncTask.doInBackground");
 	            String msg = "";
 	            try {
 	                if (gcm == null) {
@@ -94,102 +98,88 @@ public class GCM extends Extension {
 	                }
 	                regid = gcm.register(SENDER_ID);
 	                msg = "Device registered, registration ID=" + regid;
-
-	                // You should send the registration ID to your server over HTTP,
-	                // so it can use GCM/HTTP or CCS to send messages to your app.
-	                // The request to your server should be authenticated if your app
-	                // is using accounts.
-	                sendRegistrationIdToBackend();
-
-	                // For this demo: we don't need to send it because the device
-	                // will send upstream messages to a server that echo back the
-	                // message using the 'from' address in the message.
-
-	                // Persist the regID - no need to register again.
+					// Persist the regID - no need to register again.
 	                storeRegistrationId(context, regid);
 	            } catch (IOException ex) {
-	                msg = "Error :" + ex.getMessage();
-	                // If there is an error, don't just keep trying to register.
-	                // Require the user to click a button again, or perform
-	                // exponential back-off.
+					msg = "Error :" + ex.getMessage();
 	            }
 	            return msg;
 	        }
 
-//	        @Override
+			@Override
 	        protected void onPostExecute(String msg) {
-	            Log.i(TAG, "POSTEXEC: "+msg);
-	            //mDisplay.append(msg + "\n");
+				Log.i(TAG, "POSTEXEC: "+msg);
 	        }
 	    }.execute(null, null, null);
 	}
 
-	/**
-	 * Gets the current registration ID for application on GCM service.
-	 * <p>
-	 * If result is empty, the app needs to register.
-	 *
-	 * @return registration ID, or empty string if there is no existing
-	 *         registration ID.
-	 */
-	private String getRegistrationId(Context context) {
+	private static String getRegistrationId(Context context) {
 	    final SharedPreferences prefs = getGCMPreferences(context);
 	    String registrationId = prefs.getString(PROPERTY_REG_ID, "");
 	    if (registrationId.isEmpty()) {
 	        Log.i(TAG, "Registration not found.");
 	        return "";
 	    }
-	    // Check if app was updated; if so, it must clear the registration ID
-	    // since the existing regID is not guaranteed to work with the new
-	    // app version.
+		// Check if app was updated; if so, it must clear the registration ID
+		// since the existing regID is not guaranteed to work with the new
+		// app version.
 	    int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-	    int currentVersion = getAppVersion(context);
-	    if (registeredVersion != currentVersion) {
-	        Log.i(TAG, "App version changed.");
+	    if (registeredVersion != currentAppVersion) {
+	        Log.i(TAG, "App version changed. Current: "+currentAppVersion+" - registeredVersion: "+registeredVersion);
 	        return "";
 	    }
 	    return registrationId;
 	}
 
-	/**
-	 * @return Application's {@code SharedPreferences}.
-	 */
-	private SharedPreferences getGCMPreferences(Context context) {
+	private static SharedPreferences getGCMPreferences(Context context) {
 	    return mainContext.getSharedPreferences("OPENFL-GCMEX", Context.MODE_PRIVATE);
 	}
 
-	private void sendRegistrationIdToBackend() {
-    	// Your implementation here.
-	}
-
-	/**
-	 * Stores the registration ID and app versionCode in the application's
-	 * {@code SharedPreferences}.
-	 *
-	 * @param context application's context.
-	 * @param regId registration ID
-	 */
-	private void storeRegistrationId(Context context, String regId) {
+	private static void storeRegistrationId(Context context, String regId) {
+	    Log.i(TAG, "1 Saving regId: " + regId);
 	    final SharedPreferences prefs = getGCMPreferences(context);
-	    int appVersion = getAppVersion(context);
-	    Log.i(TAG, "Saving regId on app version " + appVersion);
+	    Log.i(TAG, "2 Saving regId on app version " + currentAppVersion);
 	    SharedPreferences.Editor editor = prefs.edit();
 	    editor.putString(PROPERTY_REG_ID, regId);
-	    editor.putInt(PROPERTY_APP_VERSION, appVersion);
+	    editor.putInt(PROPERTY_APP_VERSION, currentAppVersion);
 	    editor.commit();
-	}	
+	}
 
-	/**
-	* @return Application's version code from the {@code PackageManager}.
-	*/
-	private static int getAppVersion(Context context) {
+	private static void getAppVersion(Context context) {
 		try {
-		    PackageInfo packageInfo = context.getPackageManager()
-		            .getPackageInfo(context.getPackageName(), 0);
-		    return packageInfo.versionCode;
-		} catch (NameNotFoundException e) {
-		    // should never happen
-		    throw new RuntimeException("Could not get package name: " + e);
+		    PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+			currentAppVersion = packageInfo.versionCode;
+		} catch (Exception e) {
+			Log.i(TAG, "Could not get package version: " + e);
 		}
 	}
+
+
+	public static void sendMessage(String msg){
+		new AsyncTask<String,String,String>() {
+            @Override
+            protected String doInBackground(String... params) {
+                String msg = "";
+                try {
+                    Bundle data = new Bundle();
+                        data.putString("my_message", "Hello World");
+                        data.putString("my_action",
+                                "com.google.android.gcm.demo.app.ECHO_NOW");
+                        String id = Integer.toString(msgId.incrementAndGet());
+                        gcm.send(SENDER_ID + "@gcm.googleapis.com", id, data);
+                        msg = "Sent message";
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+	            Log.i(TAG, "MSGSEND - POSTEXEC: "+msg);
+            }
+        }.execute(null, null, null);
+	}
+
+
 }
--- gcmex/GCM.hx
+++ gcmex/GCM.hx
@@ -8,4 +8,25 @@ class GCM {
 		function():Bool{return false;}
 	#end
 
+	public static var getRegistrationId(default,null):Void->String=
+	#if android
+		openfl.utils.JNI.createStaticMethod("gcmex/GCM", "getRegistrationId_Static", "()Ljava/lang/String;");
+	#else
+		function():String{return null;}
+	#end
+
+	public static var init(default,null):String->Void=
+	#if android
+		openfl.utils.JNI.createStaticMethod("gcmex/GCM", "init", "(Ljava/lang/String;)V");
+	#else
+		function(senderId:String):Void{}
+	#end
+
+	public static var sendMessage(default,null):String->Void=
+	#if android
+		openfl.utils.JNI.createStaticMethod("gcmex/GCM", "sendMessage", "(Ljava/lang/String;)V");
+	#else
+		function(msg:String):Void{}
+	#end
+
 }
